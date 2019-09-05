187b6cfa632f8a8c67e47c9613e31d4d14a4cdf4
Афанасьев Андрей <an.afanasev@corp.servionica.ru>
2019-09-05 12:46:43 +0300

    changed test_items.py and conftest.py

--- test_items.py
+++ test_items.py
@@ -1,24 +1,10 @@
 from selenium import webdriver
 import pytest
-import math
 import time
 
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+def test_should_see_basket_button(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.implicitly_wait(10)
-    browser.set_page_load_timeout(10)
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-
-@pytest.mark.parametrize('url', ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
-def test_guest_should_see_login_link(browser, url):
-    link = f"https://stepik.org/lesson/{url}/step/1"
     browser.get(link)
-    answer = str(math.log(int(time.time())))
-    browser.find_element_by_css_selector(".quiz-component textarea").send_keys(answer)
-    browser.find_element_by_css_selector("button.submit-submission").click()
-    time.sleep(3)
-    assert browser.find_element_by_css_selector(".smart-hints__hint").text == "Correct!", "Test failed"
+    # time.sleep(30)
+    assert browser.find_elements_by_css_selector("button.btn-add-to-basket"), "Add to basket button is absent"
